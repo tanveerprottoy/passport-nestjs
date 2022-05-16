fe4427024257ee5a703295a0610354eb94ab0873
tanveerprottoy <tanveer.prottoy@gmail.com>
2022-05-17 00:10:55 +0600

    guard used

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from "./core/guards/jwt-auth.guard";
 
 @Controller()
+@UseGuards(JwtAuthGuard)
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
 }
